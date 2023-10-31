1579167e20e3bccc1c9ac34f31600662191b134e
Drew Richards <darichards@radford.edu>
2023-10-31 00:11:43 +0000

    Update 2 files
    - /assets/js/cookie.js
    - /index.html

---
+++ assets/js/cookie.js
@@ -0,0 +1,15 @@
+        const existingIdTokenCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('id_token='));
+        const params = window.location.hash.substring(1).split('&');
+
+        let idToken = null;
+
+        for (const param of params) {
+        if (param.startsWith('id_token=')) {
+            idToken = param.split('=')[1];
+        }
+        }
+
+        if (idToken && !existingIdTokenCookie) {
+            document.cookie = `id_token=${idToken}; path=/`;
+            window.location.replace(window.location.pathname);
+        }
