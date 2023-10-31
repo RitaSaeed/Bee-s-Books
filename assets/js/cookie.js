4771f06c3c9f5a212f1c5e67dd08f6ea4536cd34
Drew Richards <darichards@radford.edu>
2023-10-31 02:13:53 +0000

    Update file cookie.js

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -18,13 +18,47 @@
 
        printJWT(existingIdTokenCookie)
 
+        
+
+
         function printJWT(cookie) {
             const jwtParts = cookie.split('.');
             if (jwtParts.length === 3) {
                 const jwtPayload = JSON.parse(atob(jwtParts[1])); 
-                console.log("JWT Data:", jwtPayload);
+                console.log(jwtPayload);
             } else {
                 console.error("Invalid JWT format");
             }
         } 
+
+        function getCookie(name) {
+            const cookies = document.cookie.split(';');
+            for (let cookie of cookies) {
+              const [cookieName, cookieValue] = cookie.trim().split('=');
+              if (cookieName === name) {
+                return decodeURIComponent(cookieValue);
+              }
+            }
+            return null; // Return null if the cookie is not found
+        }
+
         
+        function getValueFromJWT(cookieName, fieldName) {
+            const jwtCookie = getCookie(cookieName);
+            if (jwtCookie) {
+              const parts = jwtCookie.split('.');
+              if (parts.length === 3) {
+                
+                const decodedPayload = atob(parts[1]);
+                
+                const payloadData = JSON.parse(decodedPayload);
+                
+                
+                if (payloadData[fieldName]) {
+                  return payloadData[fieldName];
+                }
+              }
+            }
+            return null; 
+          }
+          
