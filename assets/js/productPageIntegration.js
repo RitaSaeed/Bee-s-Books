6c99df6beaca6e8b5711b5a9cd8699e50c93a329
SingularityV3 <lukehulan@gmail.com>
2024-04-15 19:28:19 -0400

    Passed more data to cart table

--- assets/js/productPageIntegration.js
+++ assets/js/productPageIntegration.js
@@ -6,16 +6,31 @@ function addProductToCart(ISBN) {
         return;
     }
 
-    let eventData = JSON.stringify({
-        "payload": {
-            "Key": {"userID": username}, 
-            "Products": [ISBN]
-        }
-    });
+    const apiUrl = localStorage.getItem('productURL');
 
-    fetch("https://pxtzuwk46l.execute-api.us-east-1.amazonaws.com/dev/cart", {
-        method: 'PUT',
-        body: eventData,
-        headers: {"Content-Type": "application/json"}
+    fetch(apiUrl).then(response => {
+        if (!response.ok) {
+            throw new Error('HTTP error! Status: ${response.status}');
+        }
+
+        return response.json();
+    }).then(data => {
+        let eventData = JSON.stringify({
+            "payload": {
+                "Key": {"userID": username}, 
+                "Products": [{
+                    "ISBN": ISBN,
+                    "Title": data["Items"][0]["title"],
+                    "Image": data["Items"][0]["img"],
+                    "Price": data["Items"][0]["price"]
+                }]
+            }
+        });
+    
+        fetch("https://pxtzuwk46l.execute-api.us-east-1.amazonaws.com/dev/cart", {
+            method: 'PUT',
+            body: eventData,
+            headers: {"Content-Type": "application/json"}
+        });
     });
 }
