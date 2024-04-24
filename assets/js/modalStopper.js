5e1cd1aa1fa217ea06ed87b429d04ca151bbb5d4
Tyler Chitwood <tdchitwood@radford.edu>
2024-04-24 02:00:45 -0400

    admin dashboard bug fixes

--- assets/js/modalStopper.js
+++ assets/js/modalStopper.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-    var closeButtonClicked = false;
+// $(document).ready(function() {
+//     var closeButtonClicked = false;
 
-    // Add a click event listener to the close button
-    $('#close-add-product-icon').click(function() {
-        closeButtonClicked = true;
-        $('#add-product-modal').modal('hide');
-    });
+//     // Add a click event listener to the close button
+//     $('#close-add-product-icon').click(function() {
+//         closeButtonClicked = true;
+//         $('#add-product-modal').modal('hide');
+//     });
 
-    $('#add-product-modal').on('hide.bs.modal', function(e) {
-        if (!closeButtonClicked && $('#add-product-form')[0].checkValidity() === false) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
+//     $('#add-product-modal').on('hide.bs.modal', function(e) {
+//         if (!closeButtonClicked && $('#add-product-form')[0].checkValidity() === false) {
+//             e.preventDefault();
+//             e.stopPropagation();
+//         }
 
-        // Reset the flag
-        closeButtonClicked = false;
-    });
-});
+//         // Reset the flag
+//         closeButtonClicked = false;
+//     });
+// });
 
 
 
